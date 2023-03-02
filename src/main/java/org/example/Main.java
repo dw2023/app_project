61b7c52885859e2e442e3029a7605d97d3c8e58e
dw2023 <dwkim126@gmail.com>
2023-03-02 10:08:50 +0900

    1,2단계 완료

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,32 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 명언 앱 ==");
+
+        while (true) {
+            System.out.print("명령) ");
+            String command = sc.nextLine();
+
+            if (command.equals("종료")) {
+                break;
+            }
+
+            else if (command.equals("등록")) {
+                System.out.print("명언 : ");
+                String saying = sc.nextLine();
+
+                System.out.print("작가 : ");
+                String author = sc.nextLine();
+
+                int num = 1;
+                System.out.println(num + "번 명언이 등록되었습니다.");
+            }
+        }
+        sc.close();
     }
 }
